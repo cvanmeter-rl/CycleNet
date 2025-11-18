fb8b139998f2c6512b31cd3826c6cd59f31d33c7
Christian VanMeter <72093440+cvanmeter-rl@users.noreply.github.com>
2025-11-18 13:10:07 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,30 @@
+import cv2
+import numpy as np
+from torch.utils.data import Dataset
+from pathlib import Path
 
+class TrainDataset(Dataset):
+    def __init__(self):
+      self.data = []
+      self.data_dir = Path('/mnt/synrs3d/SynRS3D/data')
+      self.synthetic_dataset_names = [
+        "terrain_g05_mid_v1",
+        "grid_g05_mid_v2",
+        "terrain_g05_low_v1",
+        "terrain_g05_high_v1",
+        "terrain_g005_mid_v1",
+        "terrain_g005_low_v1",
+        "grid_g005_mid_v2",
+        "terrain_g005_high_v1",
+        "terrain_g1_mid_v1",
+        "terrain_g1_low_v1",
+        "terrain_g1_high_v1",
+        "grid_g005_mid_v1",
+        "grid_g005_low_v1",
+        "grid_g005_high_v1",
+        "grid_g05_mid_v1",
+        "grid_g05_low_v1",
+        "grid_g05_high_v1",
+    ]
+    for d in self.synthetic_dataset_names:
+      
