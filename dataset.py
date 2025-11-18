a84c3cb417c13b2f37b45ca6dc2c2787361e3d75
Christian VanMeter <72093440+cvanmeter-rl@users.noreply.github.com>
2025-11-18 14:17:52 -0600

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,7 +6,7 @@ from pathlib import Path
 class TrainDataset(Dataset):
     def __init__(self):
       self.data = []
-      self.data_dir = Path('/mnt/synrs3d/SynRS3D/data')
+      self.data_dir = Path('/mnt/synrs3d/SynRS3D/data/')
       self.synthetic_dataset_names = [
         "terrain_g05_mid_v1",
         "grid_g05_mid_v2",
@@ -27,4 +27,16 @@ class TrainDataset(Dataset):
         "grid_g05_high_v1",
     ]
     for d in self.synthetic_dataset_names:
-      
+        train_list = self.data_dir / d / 'train.txt'
+        with open(train_list, "r") as f:
+            image_list = [ln.strip() for ln in f if ln.strip()]
+    for image_name in image_list:
+        img_filepath = self.data_dir / d / 'opt' / f'{image_name}.tif'
+        self.data.append(img_filepath)
+
+    def __len__(self):
+        return len(self.data)
+
+    
+        
+                
