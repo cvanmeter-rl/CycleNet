9d128d05420504759cfcc71502b84b6b2055e2f0
Christian VanMeter <72093440+cvanmeter-rl@users.noreply.github.com>
2025-12-05 14:15:16 -0600

    Update train.py

--- train.py
+++ train.py
@@ -4,17 +4,19 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from dataset import TrainDataset
+from real_and_synthethic_dataset import RealAndSynthethicTrainDataset
 from cycleNet.logger import ImageLogger
 from cycleNet.model import create_model, load_state_dict
 import numpy as np
 import torch
+import argparse
 
 torch.cuda.manual_seed(21)
 np.random.seed(21)
 torch.manual_seed(21)
 
 # Configs
-resume_path = './models/single_simple_prompt_Both_False_bs4.ckpt'
+resume_path = './models/single_simple_prompt_Both_False_bs4_syn_and_real_data.ckpt'
 log_path = './logs'
 batch_size_per_gpu = 4
 gpus = 1
@@ -22,6 +24,12 @@ logger_freq = 300
 learning_rate = 1e-5
 sd_locked = False
 only_mid_control = False
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_type",type=str,required=True,help="Name of dataset being used", default="synthethic_only")
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -35,13 +43,17 @@ if __name__ == "__main__":
 
 
     # Misc
-    dataset = TrainDataset()
+    if args.dataset == "synthethic_only":
+        dataset = TrainDataset()
+    else:
+        dataset = RealAndSynthethicTrainDataset()
+        
     dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size_per_gpu, shuffle=True)
 
     #logger = ImageLogger(batch_frequency=logger_freq, every_n_train_steps=logger_freq)
     checkpoint_cb = ModelCheckpoint(
-    dirpath=f"./checkpoints/models/single_simple_prompt_Both_False_bs4/",
-    filename="step{step:06d}",
+    dirpath=f"./checkpoints/models/single_simple_prompt_Both_False_bs4_syn_and_real_data/",
+    filename="{step:06d}",
     save_top_k=-1,
     every_n_train_steps=5000,
     save_last=True,
