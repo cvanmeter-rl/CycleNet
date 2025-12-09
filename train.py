11fb4d103103fe3db142020127152e283036fa1b
Christian VanMeter <72093440+cvanmeter-rl@users.noreply.github.com>
2025-12-09 15:25:20 -0600

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ np.random.seed(21)
 torch.manual_seed(21)
 
 # Configs
-resume_path = './models/single_simple_prompt_Both_False_bs4.ckpt'
+resume_path = './models/longer_prompt_Both_False_bs4_syn_and_real_data.ckpt'
 log_path = './logs'
 batch_size_per_gpu = 4
 gpus = 1
@@ -40,10 +40,10 @@ if __name__ == "__main__":
 
     #logger = ImageLogger(batch_frequency=logger_freq, every_n_train_steps=logger_freq)
     checkpoint_cb = ModelCheckpoint(
-    dirpath=f"./checkpoints/models/single_simple_prompt_Both_False_bs4/",
+    dirpath=f"./checkpoints/models/longer_prompt_Both_False_bs4_syn_and_real_data/",
     filename="step{step:06d}",
     save_top_k=-1,
-    every_n_train_steps=5000,
+    every_n_train_steps=1000,
     save_last=True,
     monitor=None,
     )
