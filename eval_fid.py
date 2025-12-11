d5742f35e10f1f5015db6918eeeeb7afc9487d4c
Logan Brenningmeyer <loganbrenningmeyer@gmail.com>
2025-12-10 21:44:42 -0600

    setup eval_fid

--- eval_fid.py
+++ eval_fid.py
@@ -1,12 +1,12 @@
 from cleanfid import fid
 
 REAL_DIR = "/mnt/project/data/real/all"
-SIM_DIR = "/mnt/project/data/sim/all"
+SIM_DIR = "/mnt/project/data/sim/real"
 
 
 def main():
     score = fid.compute_fid(REAL_DIR, SIM_DIR, num_workers=0)
-    print(f"FID Score: {score}")
+    print(f"(real) FID Score: {score}")
 
 if __name__ == "__main__":
     main()
